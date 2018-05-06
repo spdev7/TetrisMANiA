fd2b44a9701b17e653ce146ca153cd73d8ddfd62
Saurav Prakash <sauravprakash7@outlook.com>
2018-05-06 20:12:52 +0530

    initial Build Ready

--- tetris.js
+++ tetris.js
@@ -12,7 +12,11 @@ const matrix = [
 ];
 
 function draw() {
+  context.fillStyle = '#000';
+  context.fillRect(0,0,canvas.width,canvas.height);
+
   drawMatrix(player.matrix,player.pos);
+
 }
 function drawMatrix(matrix , offset) {
   matrix.forEach((row, y) => {
@@ -26,9 +30,38 @@ function drawMatrix(matrix , offset) {
 
 }
 
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0;
+}
+let dropCounter = 0 ;
+let dropInterval = 1000;
+let lastTime = 0;
+
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+  if(dropCounter > dropInterval) {
+       playerDrop();
+  }
+  draw();
+  requestAnimationFrame(update);
+}
+
 const player = {
   pos : {x:5, y:5},
   matrix: matrix,
 }
 
-draw();
+document.addEventListener('keydown', event => {
+  if(event.keyCode === 37) {
+    player.pos.x--;
+  }else if(event.keyCode === 39) {
+    player.pos.x++;
+  }else if(event.keyCode === 40) {
+      playerDrop();
+  }
+});
+update();
